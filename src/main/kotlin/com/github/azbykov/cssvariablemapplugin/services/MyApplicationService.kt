d694a760a3648d717aba1db4a54d8f9223238f3a
GitHub Action <action@github.com>
2021-08-12 13:51:56 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/azbykov/cssvariablemapplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.azbykov.cssvariablemapplugin.services
+
+import com.github.azbykov.cssvariablemapplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
